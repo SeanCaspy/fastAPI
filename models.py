9d3973e45ba3bf916031a00ebc9f637d6f911216
Sean Caspy <seancaspy@gmail.com>
2025-04-19 21:12:36 +0300

    added tokens and database

--- models.py
+++ models.py
@@ -1,4 +1,16 @@
 from pydantic import Field, BaseModel
+from pydantic import BaseModel
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class UserDB(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
 
 class RegisterRequest(BaseModel):
     username: str
