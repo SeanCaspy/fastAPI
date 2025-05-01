e8aa5c95832d654721cfe26e09b15f238268647f
Sean Caspy <seancaspy@gmail.com>
2025-05-01 11:59:29 +0300

    change to server database

--- database.py
+++ database.py
@@ -1,13 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root@localhost/fastapi_project"
+load_dotenv()  # Load .env file
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
